f4f941a6c575d96df5241f61eec2f48594f0de2b
TanimHasanRafi <tanimhasan198@gmail.com>
2024-10-13 13:58:03 +0600

    Initial Commit

---
+++ python1.py
@@ -0,0 +1,13 @@
+#variable
+...
+name = "Tanim \n hasan"
+print(name)
+age = 24
+print(age)
+...
+#receive input
+#name = input("Enter your name:  ")
+#print("hello  " +name)
+
+name = "Rafi ccn"
+print(name)
