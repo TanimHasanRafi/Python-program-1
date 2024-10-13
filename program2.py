42a034878cd490a7c677b185fc4090d05c1ace3b
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-10-13 15:18:38 +0600

    Variable

---
+++ program2.py
@@ -0,0 +1,8 @@
+
+name = " Tanim Hasan"
+
+print("our new student name is " +name )
+print(name+ "lives in Dhaka")
+print("She is currently 24 years old")
+print("At the age of 24, she has started to learn python")
+print( name+"has scored 3.95 in her secondary exam")
