22f2edf451a5faa8c4d539e7f367b1efafdc6faf
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-10-14 18:22:37 +0600

    Math related Libray funtion

--- program5.py
+++ program5.py
@@ -6,14 +6,27 @@
 from sys import float_info
 
 # Triangle
-base = float(input(" Enter the base value :"))
-Height = float(input(" Enter the height value :"))
+# base = float(input(" Enter the base value :"))
+# Height = float(input(" Enter the height value :"))
+#
+# area = 0.5 * base * Height
+# print(area)
+#
+# # Radius
+# radius = float(input("enter the radius value : "))
+#
+# area = 3.1416 * radius * radius
+# print(area)
 
-area = 0.5 * base * Height
-print(area)
+# Math related libray funtion
 
-# Radius
-radius = float(input("enter the radius value : "))
+from math import *
 
-area = 3.1416 * radius * radius
-print(area)
+print(max(20,10))
+print(min(20,10))
+print(pow(2,3))
+print(abs(-5))
+print(sqrt(16))
+print(round(3.8))
+print(floor(2.8))
+print(ceil(2.8))
