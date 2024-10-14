fca32bdb7a8b27cb45dec0f73341ea93351b9ac4
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-10-14 18:09:13 +0600

    Area of shape

--- program5.py
+++ program5.py
@@ -1,5 +1,19 @@
-number1 = int(input("Enter the first number:"))
-number2 = int(input(" Enter the second number:"))
+# number1 = int(input("Enter the first number:"))
+# number2 = int(input(" Enter the second number:"))
+#
+# result = number1 + number2
+# print(result)
+from sys import float_info
 
-result = number1 + number2
-print(result)
+# Triangle
+base = float(input(" Enter the base value :"))
+Height = float(input(" Enter the height value :"))
+
+area = 0.5 * base * Height
+print(area)
+
+# Radius
+radius = float(input("enter the radius value : "))
+
+area = 3.1416 * radius * radius
+print(area)
