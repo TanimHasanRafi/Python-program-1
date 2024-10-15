ae9b8976ee6b833aae9f5d85ea7f74714d72c760
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-10-15 22:39:15 +0600

    if condtion math

---
+++ program6.py
@@ -0,0 +1,78 @@
+# number1 = int(input("Enter the first number:"))
+# number2 = int(input(" Enter the second number:"))
+#
+# result = number1 + number2
+# print(result)
+from sys import float_info
+
+# Triangle
+# base = float(input(" Enter the base value :"))
+# Height = float(input(" Enter the height value :"))
+#
+# area = 0.5 * base * Height
+# print(area)
+#
+# # Radius
+# radius = float(input("enter the radius value : "))
+#
+# area = 3.1416 * radius * radius
+# print(area)
+
+# Math related libray funtion
+
+# from math import *
+#
+# print(max(20,10))
+# print(min(20,10))
+# print(pow(2,3))
+# print(abs(-5))
+# print(sqrt(16))
+# print(round(3.8))
+# print(floor(2.8))
+# print(ceil(2.8))
+
+# type Funtion
+
+# num = "Tanim"
+# print(type(num))
+#
+# num = 10
+# print(type(num))
+#
+# num = 10
+# print(type(True))
+#
+# num1 = 20
+# num2 = 10
+# num3 = 10
+# # sum = num1 + num2
+# print(f"{num1} + {num2} - {num3} = {num1+num2-num3}")
+#
+# print(" Tanim Hasan",end=",")
+# print("01636068802")
+#
+# #boolean Data type & relational operation
+#
+# print(30<20)
+# print(30>20)
+# print(30>=20)
+# print(30<=20)
+# print(30==20)
+# print(30!=20)
+# print("Tanim"=="Hasan")
+# print("Tanim"=="Tanim")
+
+#if conditon
+value1 = 11
+value2 = 20
+sum= value1 + value2
+sub= value1 - value2
+
+if sum%2==0:
+     print("even")
+
+else:
+ print("odd")
+
+
+
