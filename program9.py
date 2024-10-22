8890511b73a78e94cb855dad129b005d096267ba
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-10-22 23:23:51 +0600

    Sum of N number

---
+++ program9.py
@@ -0,0 +1,15 @@
+i=0
+while i<=20:
+    print("Tanim",+i)
+    i=i+1
+
+#user input
+#2+4+6+............n
+n = int(input("Enter the value : "))
+sum = 0
+j=2
+while j<=n:
+    sum=sum+j
+    j = j + 2
+
+print(sum)
