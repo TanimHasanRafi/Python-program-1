8325bba4c3402def815c950eb54b4327992a70b0
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-10-22 23:55:00 +0600

    break & continue

---
+++ program10.py
@@ -0,0 +1,16 @@
+i=1
+while i<=100:
+    if i == 20:
+        break
+    print(i)
+    i=i+1
+print("hello")
+
+j=1
+while j<=100:
+    print(j)
+    j=j+1
+    if j == 20:
+        continue
+print("hello")
+
