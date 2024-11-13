e4c3a2daaa24df08b530166c8f0b1a7732ff0bca
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-11-13 10:52:52 +0600

    pattern printing

--- program13.py
+++ program13.py
@@ -1,15 +1,19 @@
-n=int (input("enter the last value:"))
-sum =0
+# n=int (input("enter the last value:"))
+# sum =0
+#
+# for x in range(1,n+1,1):
+#     sum=sum+x
+#     print(sum)
+#
+# #even number
+#
+# n=int (input("enter the last value:"))
+# sum =0
+#
+# for x in range(1,n+1,2):
+#     sum=sum+x
+#     print(sum)
 
-for x in range(1,n+1,1):
-    sum=sum+x
-    print(sum)
-
-#even number
-
-n=int (input("enter the last value:"))
-sum =0
-
-for x in range(1,n+1,2):
-    sum=sum+x
-    print(sum)
+n=int(input("enter the value"))
+for i in range(n+1):
+    print(i*"*")
