7f883bdac0d7303a490250759198c8bb885a7a01
Tanim Hasan <140623324+TanimHasanRafi@users.noreply.github.com>
2024-11-13 10:27:18 +0600

    Series

---
+++ program13.py
@@ -0,0 +1,15 @@
+n=int (input("enter the last value:"))
+sum =0
+
+for x in range(1,n+1,1):
+    sum=sum+x
+    print(sum)
+
+#even number
+
+n=int (input("enter the last value:"))
+sum =0
+
+for x in range(1,n+1,2):
+    sum=sum+x
+    print(sum)
